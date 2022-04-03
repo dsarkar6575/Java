f0c4fbc49dd04f727f67c3fec3def1438e1c4e38
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-03 18:41:29 +0530

    Create File Writer (Write to file )in java.java

---
+++ File Writer (Write to file )in java.java
@@ -0,0 +1,20 @@
+  package com.gcettb;
+
+  import java.io.FileWriter;
+  import java.io.IOException;
+
+  public class Main {
+
+    public static void main(String[] args) {
+try {
+    FileWriter writer= new FileWriter("poem.txt");
+    writer.write("Roses are red\n Violets are blue\n Booty booty booty booty\n Rockin' everywhere!");
+    writer.append("\n(A poem by Dipankar)");
+    writer.close();
+}
+catch (IOException e)
+{
+    e.printStackTrace();
+}
+        }
+    }
