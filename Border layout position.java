a9fc646d47f0ab03ff49ab63ed2b2f161e1dfa07
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-04 19:51:01 +0530

    Update Border layout position.java

--- Border layout position.java
+++ Border layout position.java
@@ -17,6 +17,7 @@ public class Main {
         JFrame frame=new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500,500);
+        frame.setLayout(new BorderLayout(10,10));
         frame.setVisible(true);
 
 
