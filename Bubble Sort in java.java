bd929387753135174aa15418c16f870d0495bb81
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-06 15:40:07 +0530

    Create Bubble Sort in java.java

---
+++ Bubble Sort in java.java
@@ -0,0 +1,44 @@
+package com.gcettb;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void PrintArray(int[] arr)
+    {
+        for (int i=0;i<= arr.length-1;i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int n= sc.nextInt();
+        int[] arr=new int[n];
+        System.out.println("Enter the element of array:");
+        for (int i=0;i<=n-1;i++)
+        {
+           arr[i]= sc.nextInt();
+        }
+
+        //Bubble sort
+        for(int i=1;i<arr.length-1;i++)
+        {
+            for (int j=0;j< arr.length-i-1;j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        PrintArray(arr);
+    }
+}
