486edc49110cc322fe8a7333707b689221abaaaa
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-08 15:10:37 +0530

    Create Number Pyramid pattern.java

---
+++ Number Pyramid pattern.java
@@ -0,0 +1,27 @@
+package com.gcettb;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n= sc.nextInt();
+      for (int i=1;i<=n;i++)
+      {
+
+          for (int k=1;k<=n-i;k++)   //print space
+          {
+              System.out.print(" ");
+          }
+          for (int j=1;j<=i;j++)
+          {
+              System.out.print(i+" ");
+          }
+          System.out.println();
+      }
+    }
+}
