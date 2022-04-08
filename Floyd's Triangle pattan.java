048d5474266d168a5215618a7e175ea22c2f9278
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-08 14:38:35 +0530

    Create Floyd's Triangle pattan.java

---
+++ Floyd's Triangle pattan.java
@@ -0,0 +1,25 @@
+package com.gcettb;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int n= sc.nextInt();
+        int counter=1;
+      for (int i=1;i<=n;i++)
+      {
+          for (int j=1;j<=i;j++)
+          {
+              System.out.print(counter+"  ");
+              counter++;
+          }
+          System.out.println();
+      }
+         
+    }
+}
