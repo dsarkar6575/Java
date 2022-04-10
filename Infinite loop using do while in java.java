8318c5d9e1781b3591d9cd1408d23978b254e12b
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-10 13:32:47 +0530

    Create Infinite loop using do while in java.java

---
+++ Infinite loop using do while in java.java
@@ -0,0 +1,15 @@
+package com.gcettb;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+      do{
+          System.out.println(1);
+      }while (true);
+
+    }
+}
