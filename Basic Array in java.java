6a67b09f23c7c3c50fcc01e901b22739952eb2fc
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-11 06:01:44 +0530

    Create Basic Array in java.java

---
+++ Basic Array in java.java
@@ -0,0 +1,16 @@
+package com.gcettb;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] marks=new int[3];
+        marks[0]=97;
+        marks[1]=98;
+        marks[2]=99;
+      for(int i:marks){
+          System.out.println(i);
+      }
+    }
+}
