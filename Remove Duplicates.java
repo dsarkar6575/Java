3a8c93adf00677b9daee7b8056e2a648e68a9bd9
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-13 14:13:14 +0530

    Create Remove Duplicates.java

---
+++ Remove Duplicates.java
@@ -0,0 +1,30 @@
+package com.gcettb;
+
+import java.util.*;
+
+public class Main {
+   public static boolean[] map=new boolean[26];
+   public static void removeDuplicates(String str,int idx,String newStr)
+   {
+       if (idx==str.length())
+       {
+           System.out.println(newStr);
+           return;
+       }
+       char currChar=str.charAt(idx);
+       if (map[currChar-'a']==true)
+       {
+           removeDuplicates(str,idx+1,newStr);
+       }else {
+           newStr+=currChar;
+           map[currChar-'a']=true;
+           removeDuplicates(str,idx+1,newStr);
+       }
+   }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         String str=sc.nextLine();
+         removeDuplicates(str,0,"");
+
+    }
+}
