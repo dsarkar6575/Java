8d21c61733d27e9351c6b5d5791ba67883453aa2
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-13 12:59:27 +0530

    Create Sorted disasending or not using recursion.java

---
+++ Sorted disasending or not using recursion.java
@@ -0,0 +1,36 @@
+package com.gcettb;
+
+import java.util.*;
+
+public class Main {
+
+
+    public static boolean issorted(int arr[], int idx) {
+        if (idx == arr.length - 1) {
+            return true;
+        }
+        if (arr[idx] > arr[idx + 1]) {
+            //array is sorted till now
+            issorted(arr, idx + 1);
+        } else {
+            return false;
+        }
+        return arr[idx]>arr[idx+1];
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int []arr=new int[n];
+        for (int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println(issorted(arr,0));
+
+
+    }
+}
