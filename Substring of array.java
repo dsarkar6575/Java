d79d86d62f8d304cf043c08b1f8731100d3845d1
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-04-28 01:20:49 +0530

    Create Substring of array.java

---
+++ Substring of array.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+          String s1= str.substring(0,str.indexOf("@"));
+               System.out.print(s1);
+              
+    }
+    
+}
