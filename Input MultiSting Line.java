3e585edba583eec1807f4059842826ec7c199aaa
Dipankar Sarkar <60103089+dsarkar6575@users.noreply.github.com>
2022-05-25 12:04:06 +0530

    Create Input MultiSting Line.java

---
+++ Input MultiSting Line.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class create {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        double d=sc.nextDouble();
+        sc.nextLine();
+        String s=sc.nextLine();
+        String s1=sc.nextLine();
+        sc.close();
+        
+
+        
+        System.out.println("String: "+ s);
+        System.out.println("Double: "+d);
+        System.out.println("Int: "+n);
+        System.out.println(s1);
+    }
+}
